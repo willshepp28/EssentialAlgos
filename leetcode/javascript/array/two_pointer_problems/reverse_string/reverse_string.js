762918aaa5e3447c62e2bf2bf621f25f76bc69bf
willshepp28 <willsheppard29@gmail.com>
2023-09-02 09:10:17 -0500

    updated variable names in reverse string and tests for readablity

--- leetcode/javascript/array/two_pointer_problems/reverse_string/reverse_string.js
+++ leetcode/javascript/array/two_pointer_problems/reverse_string/reverse_string.js
@@ -1,21 +1,21 @@
 
-const reverseString = function(inputString) {
+
+
+const reverseString = function(arrayOfStrings) {
     // Create pointers
     let startIndex = 0;
-    let endIndex = inputString.length - 1;
+    let endIndex = arrayOfStrings.length - 1;
 
     // Iterate through the string
     while (startIndex < endIndex) {
         // Swap characters at indexes
-        let temporaryChar = inputString[startIndex];
-        inputString[startIndex] = inputString[endIndex];
-        inputString[endIndex] = temporaryChar;
+        let temporaryChar = arrayOfStrings[startIndex];
+        arrayOfStrings[startIndex] = arrayOfStrings[endIndex];
+        arrayOfStrings[endIndex] = temporaryChar;
 
         startIndex++;
         endIndex--;
     }
-
-    console.log(inputString)
 };
 
 module.exports = reverseString;
--- leetcode/javascript/array/two_pointer_problems/reverse_string/reverse_string.test.js
+++ leetcode/javascript/array/two_pointer_problems/reverse_string/reverse_string.test.js
@@ -1,15 +1,15 @@
 const reverseString = require("./reverse_string");
 
-test('Reverses an empty sting', () => {
-    let input = []
-    reverseString(input);
-    expect(input).toStrictEqual([])
+test('Reverses an empty arry', () => {
+    let arrayOfStrings = []
+    reverseString(arrayOfStrings);
+    expect(arrayOfStrings).toStrictEqual([])
 });
 
-test('Reverse a longer string', () => {
-    let inputArr = ["h","e","l","l","o"];
+test('Reverse a longer array of strings', () => {
+    let arrayOfStrings = ["h","e","l","l","o"];
 
-    reverseString(inputArr);
-    console.log(inputArr)
-    expect(inputArr).toStrictEqual(["o","l","l","e","h"]);
+    reverseString(arrayOfStrings);
+    console.log(arrayOfStrings)
+    expect(arrayOfStrings).toStrictEqual(["o","l","l","e","h"]);
 })
