e70588ba577a1bf54f75e904a21a1c9786c77b39
willshepp28 <willsheppard29@gmail.com>
2023-09-11 19:59:47 -0500

    grouped and renamed tests for readablity

--- leetcode/javascript/array/fizzbuzz/fizzbuzz.test.js
+++ leetcode/javascript/array/fizzbuzz/fizzbuzz.test.js
@@ -1,5 +1,5 @@
 const originalFizzBuzz = require("./fizzbuzz");
-const fizzBuzzV2 = require("./fizzbuzz.v2/fizzbuzz.v2");
+const fizzBuzzVersion2 = require("./fizzbuzz.v2/fizzbuzz.v2");
 
 const checkReturnsEmptyArray = (func) => {
     let arr = func();
@@ -16,18 +16,23 @@ describe('FizzBuzz Tests', () => {
         });
     })
 
-    test('Returns ["1", "2", "Fizz"] when given 3', () => {
-        const arr = originalFizzBuzz(3);
-        expect(arr).toStrictEqual(["1", "2", "Fizz"]);
+    describe('Output for Input 3', () => {
+        test('Original version should return ["1", "2", "Fizz"] when given 3', () => {
+            const arr = originalFizzBuzz(3);
+            expect(arr).toStrictEqual(["1", "2", "Fizz"]);
+        });
+    
+        test('Version 2 should return ["1", "2", "Fizz"] when given 3 for v2', () => {
+            const arr = fizzBuzzVersion2(3);
+            expect(arr).toStrictEqual(["1", "2", "Fizz"]);
+        });
     });
 
-    test('Returns ["1", "2", "Fizz"] when given 3 for v2', () => {
-        const arr = fizzBuzzV2(3);
-        expect(arr).toStrictEqual(["1", "2", "Fizz"]);
-    });
+    describe('Output for Input 15', () => {
+        test('Should return the expected FizzBuzz sequence up to 15', () => {
+            const arr = fizzBuzzVersion2(15);
+            expect(arr).toStrictEqual(["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]);
+        });
+    })
 
-    test('Returns appropriate values when given 15 for v2', () => {
-        const arr = fizzBuzzV2(15);
-        expect(arr).toStrictEqual(["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]);
-    });
 });
