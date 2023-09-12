eac1f75a22446205368bc6ed82ee313eca11fb54
willshepp28 <willsheppard29@gmail.com>
2023-09-11 19:55:43 -0500

    renamed returnsEmptyArr to checkReturnsEmptyArray;

--- leetcode/javascript/array/fizzbuzz/fizzbuzz.test.js
+++ leetcode/javascript/array/fizzbuzz/fizzbuzz.test.js
@@ -1,16 +1,17 @@
 const fizzBuzz = require("./fizzbuzz");
 const fizzBuzzV2 = require("./fizzbuzz.v2/fizzbuzz.v2");
 
-const returnsEmptyArr = (func) => {
+const checkReturnsEmptyArray = (func) => {
     let arr = func();
     expect(arr).toStrictEqual([]);
 };
 
 describe('FizzBuzz Tests', () => {
     test.each([
-        [fizzBuzz, 'Returns empty array for fizzBuzz'],
+        [fizzBuzz, 'Returns an empty array when given no values'],
+        [fizzBuzzV2, 'Returns an empty array when given no values']
     ])('%s', (func, description) => {
-        returnsEmptyArr(func);
+        checkReturnsEmptyArray(func);
     });
 
     test('Returns ["1", "2", "Fizz"] when given 3', () => {
@@ -21,5 +22,10 @@ describe('FizzBuzz Tests', () => {
     test('Returns ["1", "2", "Fizz"] when given 3 for v2', () => {
         const arr = fizzBuzzV2(3);
         expect(arr).toStrictEqual(["1", "2", "Fizz"]);
+    });
+
+    test('Returns ["1", "2", "Fizz"] when given 3 for v2', () => {
+        const arr = fizzBuzzV2(15);
+        expect(arr).toStrictEqual(["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]);
     });
 });
